93056ef0a92dc3e141ac4901b06445d18d79a12e
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-02-26 09:24:41 +0100

    addDoc() create note

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,29 +1,85 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  doc,
+  collectionData,
+  onSnapshot,
+  addDoc,
+} from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  normalNotes: Note[] = [];
+  trashNotes: Note[] = [];
 
   firestore = inject(Firestore);
-  items$;
+
+  unsubNotes;
+  unsubTrash;
 
   constructor() {
-    this.items$ = collectionData(this.getNoteslRef());
-   }
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
+  }
 
-  getNoteslRef(){
+  async addNote(item: Note) {
+    await addDoc(this.getNotesRef(), item)
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef?.id);
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach((element) => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+      // console.log(this.trashNotes);
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach((element) => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+      console.log(this.normalNotes);
+    });
+  }
+
+  getNotesRef() {
     return collection(this.firestore, 'notes');
   }
 
-  getTrashRef(){
+  getTrashRef() {
     return collection(this.firestore, 'trash');
   }
 
-  getSingleDocRef(colId: string, docId: string){
+  getSingleDocRef(colId: string, docId: string) {
     return doc(collection(this.firestore, colId), docId);
   }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id || '',
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    };
+  }
 }
-
-
